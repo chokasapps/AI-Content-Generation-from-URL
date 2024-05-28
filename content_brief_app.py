65b1be7ed26ddedfff693719a1387438c0b0f1f7
chokasapps <chokasapps@gmail.com>
2024-05-28 18:51:42 +0500

    tried to increase outpput tokens

--- content_brief_app.py
+++ content_brief_app.py
@@ -89,7 +89,7 @@ def generate_content(prompt, openai_api_key):
     ]"""
     client = OpenAI(api_key=openai_api_key)
     response = client.chat.completions.create(
-        model="gpt-3.5-turbo", messages=prompt, temperature=0.7
+        model="gpt-3.5-turbo", messages=prompt, temperature=0.7, max_tokens=1000
     )
     return response.choices[0].message.content
 
